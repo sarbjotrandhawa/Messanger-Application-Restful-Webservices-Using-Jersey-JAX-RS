ce720e0b11f3286099f80396e149765a6dad2ba0
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2021-05-13 14:39:10 -0400

    Commit

--- src/main/java/org/sarb/java/messanger/resources/MessageResource.java
+++ src/main/java/org/sarb/java/messanger/resources/MessageResource.java
@@ -5,8 +5,9 @@ import java.util.List;
 import org.sarb.java.messanger.model.Message;
 import org.sarb.java.messanger.service.MessageService;
 
-import jakarta.ws.rs.GET;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -19,6 +20,35 @@ public class MessageResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
+	}
+
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message getMessage(@PathParam("messageId") long id) {
+		return messageService.getMessage(id);
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
+	}
+
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
 	}
 
 }
